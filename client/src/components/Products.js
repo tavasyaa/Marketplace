fd3e7bad4d553352d9083accf30ef64c3f1b74de
Tavasya Agarwal <TavasyaAgarwal2020@u.northwestern.edu>
2020-04-13 17:13:54 -0500

    now gets data from mysql, have to properly format and display in products.js

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -1,6 +1,27 @@
 import React from 'react';
 
 class Products extends React.Component {
+	state = {
+		data: []
+	};
+
+
+	componentDidMount() {
+		this.callBackend()
+			.then(res => this.setState({ data: res.express }))
+			.catch(err => console.log(err))
+	}
+
+	callBackend = async () => {
+		const response = await fetch('/express_backend');
+		const body = await response.json();
+
+		if (response.status !== 200) {
+			throw Error(body.message) 
+		}
+		return body;
+};
+
 	render() {
 		return(
 			<div className="jumbotron" style={{margin: 0}}>
@@ -8,7 +29,7 @@ class Products extends React.Component {
 					<h2>Search</h2>
 				</div>
 				<div className="col-sm-9">
-					sample text
+					{console.log(this.state.data[0])}
 				</div>
 			</div>
 		)
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: 'novakdjokovic1',
+	database: 'marketplace'
+});
+
+connection.connect(function(err){
+	(err)? console.log(err): console.log(connection);
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+ app.get('/express_backend', (req, res) => {
+	connection.query('select * from items', function(err, data){
+		err ? res.send(err) : res.send({express: data})
+	})
+})
